0cc9a9c18a84d097efb46c39564a6c4a44cd324d
deb-cmd <web.noxus@gmail.com>
2025-03-22 21:07:46 +0530

    Initial commit

---
+++ hello-react/src/App.jsx
@@ -0,0 +1,27 @@
+import TaskCard from "./TaskCard"
+
+function App() {
+
+  return (
+    <div>
+      <div>
+        <h1>
+          Pending
+        </h1>
+        <TaskCard title="Build the website with static content">
+
+        </TaskCard>
+      </div>
+      <div>
+        <h1>
+          Done
+        </h1>
+        <TaskCard title="Design the mockup">
+
+        </TaskCard>
+      </div>
+    </div>
+  )
+}
+
+export default App
